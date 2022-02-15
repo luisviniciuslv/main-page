d142c20052960b2b66aba62493db3b45e3b69720
Luis Vinicius <camperqueodeiacamper@gmail.com>
2022-02-15 12:46:19 -0300

    adjust background system

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 var d = new Date();
 var mouth = new Date(d).getMonth() + 1
 const date = d.toDateString()
-const date2 = d.getDate()+'/' + mouth + '/'+d.getFullYear()
- 
- document.getElementById('date').innerHTML = date2
+const date2 = d.getDate()+'/'+mouth +'/'+d.getFullYear()
+let body = document.getElementById('body');
+meuStorage = localStorage;
+document.getElementById('date').innerHTML = date2
 
- 
+meuStorage.getItem('reso') ? height = parseInt(meuStorage.getItem('reso')) : height = 100
+body.style.height = height + 'vh';
+function downFunction(){
+  height -= 3
+  body.style.height = height + 'vh';
+  localStorage.setItem('reso', height);  
+ }
+function upFunction(){
+  height += 3
+  body.style.height = height + 'vh';
+  localStorage.setItem('reso', height);  
+}
