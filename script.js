a7f0191f343d4fc5799b7edd2b62b326a2acb5e1
Luis Vinicius <camperqueodeiacamper@gmail.com>
2022-02-15 12:48:13 -0300

    background system adjust

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ document.getElementById('date').innerHTML = date2
 meuStorage.getItem('reso') ? height = parseInt(meuStorage.getItem('reso')) : height = 100
 body.style.height = height + 'vh';
 function downFunction(){
-  height -= 3
+  height -= 2
   body.style.height = height + 'vh';
   localStorage.setItem('reso', height);  
  }
 function upFunction(){
-  height += 3
+  height += 2
   body.style.height = height + 'vh';
   localStorage.setItem('reso', height);  
 }
